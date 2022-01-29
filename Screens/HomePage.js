3a3f3b0668109c84a1cbb81cfcc8a6b2e4b92ab6
Furkan Ayilmaz <furkanayilmaz@Gmail.com>
2022-01-29 16:25:41 -0700

    Added create tweet button

--- Screens/HomePage.js
+++ Screens/HomePage.js
@@ -1,18 +1,40 @@
-import { SafeAreaView, StyleSheet, View, ScrollView } from "react-native"
+import { SafeAreaView, StyleSheet, ScrollView, RefreshControl } from "react-native"
 import Header from '../components/Home/Header';
 import Posts from "../components/Home/Post";
 import { POSTS } from "../data/posts";
+import CreatePostButton from '../components/Home/CreatePostButton';
+import React, { useState } from "react";
+
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
 const HomePage = () => {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(2000).then(() => setRefreshing(false));
+    }, []);
+
     return (
         <SafeAreaView style={style.container}>
             <Header />
 
             <ScrollView>
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
                 {POSTS.map((post, index) => (
                     <Posts post={post} key={index} />
                 ))}
             </ScrollView>
+
+            {/* <View style={{ zIndex: 999}}>
+                <TouchableOpacity style={{ borderRadius: 50, backgroundColor: "#2c8fee", width: 50, height: 50 }}>
+                    <Image source={{ uri: "https://img.icons8.com/material-outlined/60/758490/filled-topic.png"}} style={{ width: 20, height: 20}} />
+                </TouchableOpacity>
+            </View> */}
+
+            <CreatePostButton />
 
         </SafeAreaView>
 
---
+++ components/Home/CreatePostButton.js
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+
+const CreatePostButton = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.button}>
+                <TouchableOpacity style={{ width: 50, height: 50}}>
+                    <Image source={{ uri: "https://img.icons8.com/android/344/ffffff/plus.png" }} style={{ right: 5, bottom: 5, width: 20, height: 20 }}/>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        zIndex: 999,
+        bottom: 100,
+        right: 10,
+    },
+    button: {
+        borderRadius: 50,
+        backgroundColor: "#2c8fee",
+        padding: 20,
+        width: 50,
+        height: 50,
+    }
+})
+
+export default CreatePostButton;
